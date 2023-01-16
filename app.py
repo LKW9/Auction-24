8eadc3077b6a00ab4289bb434080e4261acdd31d
0hyeon <djdjdjk2006@naver.com>
2023-01-16 14:27:42 +0900

    first push

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route("/home", methods=["POST"])
 def bucket_post():
