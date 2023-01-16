55fa074eafa83130a43d363f827f014f14c1699e
DESKTOP-31PUNA4\whdud <whdudwns7777@naver.com>
2023-01-16 20:16:39 +0900

    feat: Add method for getitemlist()

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.r95aysd.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-
+###HOME###
 @app.route('/')
 def home():
+    ##jwt 인증 토큰이 없으면 로그인 페이지 렌더링
+    ##jwt 인증 토큰이 있으면 main 렌더링
     return render_template('main.html')
 
+###AUTH###
 @app.route('/auth/login')
 def login():
     return render_template('/auth/login.html')
@@ -18,27 +21,24 @@ def login():
 def join():
     return render_template('/auth/join.html')
 
+###MAIN###
+@app.route("/items", methods=["GET"])
+def getItemList():
+    users = db.users.find({})
+    allItems = []
+    for user in users:
+        items = user['items']
+        for num in range(len(items)):
+            allItems.append(items[num])
+    return jsonify({'allItems': allItems})
+
+
+
+###Modify page###
 @app.route('/detail/<int:id>')
 def detail(id):
+
     return render_template('/detail.html',id=id)
-
-
-
-@app.route("/home", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    bucket_list = list(db.bucket.find({}, {'_id' : False}))
-    count = len(bucket_list) + 1
-
-    doc = {
-        'title'
-    }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'POST(기록) 연결 완료!'})
-
-@app.route("/home", methods=["GET"])
-def bucket_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
 
 
 ##item upload##
@@ -48,22 +48,26 @@ def uploadItem(id):
     title = request.form['title']
     pic = request.form['pic']
     minBid = request.form['minBid']
+    unitBid = request.form['unitBid']
     desc = request.form['desc']
     status = request.form['status']
+    owner = id
     num = len(items) + 1
     item = {
         'itemNum' : num+1,
         'title' : title,
         'pic' : pic,
         'minBid' : minBid,
+        'unitBid' : unitBid,
         'status' : status,
-        'desc' : desc
+        'desc' : desc,
+        'owner' : owner
     }
     items.append(item)
     db.users.update_one({'id': id}, {'$set': {'items': items}})
     return jsonify({'msg' : '등록 완료!'})
 
-### 안녕하세요 김머현입니다###
+
 
 
 
