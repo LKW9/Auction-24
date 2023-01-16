2edd8cfac211245cf7ffae82909a3542cedb8563
choincnp <whdudwns7777@gmail.com>
2023-01-17 02:29:23 +0900

    feat: Login기능 구현, navbar 바꾸기

--- app.py
+++ app.py
@@ -1,30 +1,109 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
+from flask import Flask, session, render_template, request, jsonify
+import requests
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.r95aysd.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-id='abcd'
+app = Flask(__name__)
+app.secret_key = "Mykey"
+
+sessionId = 0
+
+# API
+# CONSTANT
+REQ = {
+    'KEY': 'User-Agent',
+    'VALUE': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+}
+
+###Sessions###
+def checkSessionValidation():
+  if session['id']:
+    global sessionId;
+    sessionId = session['id']
+    return
+  return render_template('/auth/login.html')
+
 
 ###HOME###
 @app.route('/')
 def home():
-    ##jwt 인증 토큰이 없으면 로그인 페이지 렌더링
-    ##jwt 인증 토큰이 있으면 main 렌더링
-    return render_template('main.html')
+    if 'id' in session:
+        global sessionId
+        print(sessionId)
+        return render_template('main.html')
+    else:
+        print(sessionId)
+        return render_template('/auth/login.html')
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
 
 ###AUTH###
-@app.route('/auth/login')
-def login():
+@app.route('/auth/login', methods=['GET'])
+def getlogin():
     return render_template('/auth/login.html')
 
-@app.route('/auth/join')
+@app.route('/auth/login', methods=['POST'])
+def logIn():
+    user_id = request.form['inputId']
+    user_pw = request.form['inputPw']
+    error = True
+    # 유효성 검사 (빈문자열, 4자리 미만, ID 존재여부 -> 존재하면 PW 일치 확인)
+    if (len(user_id.strip()) == 0):
+        msg = "ID를 입력해 주세요."
+    elif (len(user_pw.strip()) == 0):
+        msg = "PW를 입력해 주세요."
+    elif (len(user_id) < 4 or len(user_pw) < 4):
+        msg = "ID와 PW의 길이는 4자가 넘어야 합니다."
+    elif (db.users.find_one({'id': user_id}) == None):  # 등록되지 않은 ID
+        msg = "등록되지 않은 ID입니다. 회원가입 하세요."
+    elif (db.users.find_one({'id': user_id})['pw'] != user_pw):  # ID, PW 일치 확인
+        msg = "ID와 PW가 일치하지 않습니다. 다시 확인해보세요."
+    else:
+        msg = "로그인 성공"
+        error = False  # 로그인 가능
+        session['id'] = user_id
+        global sessionId
+        sessionId = user_id
+
+    return jsonify({'message': msg, 'error': error})
+
+@app.route('/auth/join', methods=['GET'])
 def join():
     return render_template('/auth/join.html')
+
+@app.route('/auth/signIn', methods=['POST'])
+def signIn():
+    inputId = request.form['inputId']
+    inputPw = request.form['inputPw']
+    error = True
+    # 유효성 검사 (빈문자열, 4자리 미만, ID중복 여부)
+    if (len(inputId.strip()) == 0):
+        msg = "ID를 입력해 주세요."
+    elif (len(inputPw.strip()) == 0):
+        msg = "PW를 입력해 주세요."
+    elif (len(inputId) < 4 or len(inputPw) < 4):
+        msg = "ID와 PW의 길이는 4자가 넘어야 합니다."
+    elif (db.users.find_one({'id': inputId}) != None):  # 이미 등록된 ID
+        msg = "이미 등록된 ID입니다."
+    else:
+        msg = "회원가입 완료!"
+        error = False
+        db.users.insert_one({'id': inputId, 'pw': inputPw})
+    return jsonify({'message': msg, 'error': error})
+
+#Logout
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('id', None)
+    return jsonify({'message': "로그아웃."})
 
 @app.route('/upload', methods=["GET"])
 def upload():
     return render_template('/upload.html')
+
 
 ###MAIN###
 @app.route("/items", methods=["GET"])
@@ -36,6 +115,7 @@ def getItemList():
 ##Upload##
 @app.route("/upload" , methods=["POST"])
 def uploadItem():
+    checkSessionValidation()
     itemList = list(db.bucket.find({}, {'_id': False}))
     itemNum = len(itemList) + 1
     title = request.form['title']
@@ -45,7 +125,7 @@ def uploadItem():
     unitBid = request.form['unitBid']
     desc = request.form['desc']
     status = request.form['status']
-    owner = id
+    owner = sessionId
     item = {
         'itemNum' : itemNum,
         'title' : title,
